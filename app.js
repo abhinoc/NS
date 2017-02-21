a17c6cc47959ab3157a031b1fb5e4cc3578a6001
Abhishek Alevoor <Abhishek.Alevoor@citrix.com>
2017-02-21 13:37:04 +0100

    socket.broadcast.emit:- display new user a welcome message and update the other clients

--- app.js
+++ app.js
@@ -10,10 +10,12 @@ var clients = 0;
 
 io.on('connection', function(socket){
     clients++;
-  io.sockets.emit('broadcast', { description: clients + ' clients connected!'});
+  io.sockets.emit('broadcast',{ description: clients + ' clients connected!'});
+  socket.emit('newclientconnect', { description: 'Hey, welcome!'});
+  socket.broadcast.emit('newclientconnect' ,{ decription: clients + ' clients connected!'}) 
   socket.on('disconnect', function () {
                clients--;
-                  io.sockets.emit('broadcast', { description: clients + ' clients connected!'});
+                  socket.broadcast.emit('newclientconnect', { description: clients + ' clients connected!'});
                });
 });
 
