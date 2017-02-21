783a9b301e1ab360cf5eed2f08b9910d6ebd3ae5
Abhishek Alevoor <Abhishek.Alevoor@citrix.com>
2017-02-21 13:15:13 +0100

    NS Project

---
+++ app.js
@@ -0,0 +1,29 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('A user connected');
+
+  //Send a message after a timeout of 4seconds
+  setTimeout(function(){
+    socket.emit('testerEvent', { description: 'A custom event named testerEvent!'});
+  }, 4000);
+  socket.on('disconnect', function () {
+    console.log('A user disconnected');
+  });
+});
+
+io.on('connection', function(socket){
+   socket.on('clientEvent', function(data){
+        console.log(data);
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
